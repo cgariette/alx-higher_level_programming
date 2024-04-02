e7ac334574ee0a3de23065137c65b774c4ac0d2c
cgariette <clifftonafande@gmail.com>
2024-04-02 13:08:12 -0700

    3

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -3,7 +3,7 @@ def safe_print_division(a, b):
     try:
         result = a / b
     except(FloatingPointError, ZeroDivisionError):
-        div = None
+        result = None
     finally:
         print("Inside result: {}".format(result))
 
